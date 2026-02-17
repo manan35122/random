ef55404af6b69635977c258c41ee89204fcc1e93
mananarshad <mananarshad351@gmail.com>
2026-02-17 12:20:29 +0500

    UI changes

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -5,7 +5,7 @@ import { ChevronLeft, ChevronRight, Sparkles, Star, Zap, PartyPopper } from "luc
 import { FloatingEmoji } from "@/components/FloatingEmoji";
 import { ConfettiEffect } from "@/components/ConfettiEffect";
 
-const SITE_STATUS: number = 0;
+const SITE_STATUS: number = 1;
 
 const reasons = [
   "You are one of the most closest person to me, if i exclude my family you are the only one who knows the most about me.",
